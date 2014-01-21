6ac9bcff81ed4c2c8b2ccaa25da7761448232c58
Ian Kronquist <iankronquist@teleport.com>
2014-01-20 20:24:52 -0800

    Add more type templates to llelement

--- llelement.cpp
+++ llelement.cpp
@@ -1,13 +1,37 @@
 #include "llelement.hpp"
 
-
-
+/* 
+This constructor takes the given elements and values and
+The template must be declared before the implementation of the method
+and the llelement must take a specific type T specified at compile time such as:
+	llelement<int>* element = new llelement<int>(3.14, NULL, NULL);
+*/
 template <typename T>
 llelement<T>::llelement(T value, llelement* prevElem, llelement* nextElem)
 {
         this->prev = prevElem;
         this->next = nextElem;
+	this->value = value;
 }
 
+// The following templates define what types can be held as the values in
+// llelement objects. 
+// These are the standard integer types both signed and unsigned.
 template class llelement<int>;
+template class llelement<short>;
+template class llelement<long>;
+template class llelement<unsigned int>;
+template class llelement<unsigned short>;
+template class llelement<unsigned long>;
+// The following are the standard floating point types
+template class llelement<float>;
+template class llelement<double>;
+// Booleans
+template class llelement<bool>;
+// String types
+template class llelement<std::string>;
+template class llelement<char*>;
+// void pointers which may be cast as pointers to whatever object you need.
+template class llelement<void*>;
+
 
--- llelement.hpp
+++ llelement.hpp
@@ -1,19 +1,24 @@
 #ifndef LLELEMENT_HPP
 #define LLELEMENT_HPP
-//#include "llist.hpp"
+// For those who would like an llelemnt which holds a C++ style string.
+#include <string>
 
+// Declare a template for the type T. The specific possible types may be found 
+// in the implementation.
 template <typename T>
 class llelement
 {
 	public:
+		// Constructor for a new llelement which takes values to 
+		// initialize all members.
 		llelement(T value, llelement* prevElem, llelement* nextElem);
-		//llist* list;
+		// The next llelement in the list.
 		llelement* next;
+		// The previous llelement in the list.
 		llelement* prev;
+		// The specific value for this element.
+		// This value is of type T, where T is declared in the source.
 		T value;
 };
-
-
-
 #endif
 
