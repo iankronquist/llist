623cbecb1d5d879913c71c49bc4fac964d1d364f
Ian Kronquist <iankronquist@teleport.com>
2014-01-20 20:38:28 -0800

    Seperate llelement tests and llist tests

--- llistTests.cpp
+++ llistTests.cpp
@@ -1,37 +1,25 @@
-//#include "llist.h"
-//#include "llist.cpp"
-#include "llelement.hpp"
-#include <stdio.h>
-#include <assert.h>
-#include <stdbool.h>
+#include "llist.hpp"
+#include <cstdio>
+#include <cassert>
 
-bool testElementCreation();
+bool testllistCreationAndDestruction();
 int main()
 {
-	int passed = true;
-	//passed = testllistCreationAndDestruction();
-	passed = testElementCreation();
+	bool passed = true;
+	
+	passed = testllistCreationAndDestruction();
 	return 0;
 }
 
-bool testElementCreation()
-{
-	puts("a");
-	llelement<int>* elem = new llelement<int>(10, NULL, NULL);
-	puts("b");
-	delete elem;
-	return true;
-}
 
-/*
-int testllistCreationAndDestruction()	
+bool testllistCreationAndDestruction()	
 {
 	int array[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	//struct llist* arr = llistCreate();
-	llist* arrdanlist = new llist(array, 10);
+	llist<int>* arrdanlist = new llist<int>(array, 10);
 	printf("Testing the llistDestroy function\n"); 
 	//llistDestroy(arr);
-	~llist(arrdanlist);
+	delete arrdanlist;
 	printf("Test passed\n");
 	return 1;
-}*/
+}
